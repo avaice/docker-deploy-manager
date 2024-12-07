e2ac573ad951fa3c9199de9e9dcf290b8c27b7b0
avaice <dev@cho-ice.xyz>
2024-12-08 02:50:48 +0900

    fix lint

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,12 @@
-import { ServerActionsSection } from "./_components/ServerActionsSection"
-import { StreamSection } from "./_components/StreamSection";
-
 export default function Home() {
-
-  
-
   return (
-    <div className="p-4 max-w-[800px] w-full mx-auto">
+    <div className="mx-auto w-full max-w-[800px] p-4">
       <header>
-      <h1 className="text-4xl">Next.js + Python</h1>
-      <p>Next.js+Pythonのテスト</p>
+        <h1 className="text-4xl">Next.js + Python</h1>
+        <p>Next.js+Pythonのテスト</p>
       </header>
-      
-      <div className="py-4 flex flex-col gap-2">
-        <ServerActionsSection />
-        <StreamSection />
-      </div>
+
+      <div className="flex flex-col gap-2 py-4"></div>
     </div>
   );
 }
